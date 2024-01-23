63fa03a52b753311f5ed575a11d0c1b3dba5ab13
Ali Gates <hacktech877@gmail.com>
2024-01-23 12:20:38 +0500

    8th Updation

---
+++ context-api/src/Context/UserContext.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UserContext =  React.createContext()
+
+/* React.createContext() is a function provided by the React library in JavaScript. 
+It is used to create a context object, which can then be used to share values 
+(such as state or functions) between components in a React application without 
+having to pass the props manually at every level of the component tree.
+React.createContext() is a function provided by the React library in JavaScript. 
+It is used to create a context object, which can then be used to share values 
+(such as state or functions) between components in a React application without 
+having to pass the props manually at every level of the component tree.
+ */
+
+export default UserContext;
